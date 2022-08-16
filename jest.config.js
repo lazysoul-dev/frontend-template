cab004f4e6e95a9aa850a68395bcc22c4f0f9156
lazysoul-dev <lazysoul.dev@yandex.ru>
2022-08-16 10:38:48 +0000

    chore: remove verbose output from jest

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   clearMocks: true,
   testEnvironment: "@happy-dom/jest-environment",
-  verbose: true,
   transform: {
     "\\.[jt]s?$": "babel-jest",
     "\\.vue$": "@vue/vue3-jest",
